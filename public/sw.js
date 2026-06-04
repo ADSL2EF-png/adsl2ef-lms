0ce036c3d4c40374ce6fb0ca5f13ac6cb7c72d6c
adsllome-glitch <265276406+adsllome-glitch@users.noreply.github.com>
2026-06-04 17:05:14 +0000

    Improve founder photo fit on mobile

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * VERSION : 2.0.0
  */
 
-const CACHE_NAME = "adsl2ef-v9-social-footer";
+const CACHE_NAME = "adsl2ef-v10-founder-photo-fit";
 const OFFLINE_URL = "/offline.html";
 
 const PRECACHE_ASSETS = [
