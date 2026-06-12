54018f01d53de040669a688a656faad575628b3a
adsllome-glitch <265276406+adsllome-glitch@users.noreply.github.com>
2026-06-12 05:38:46 +0000

    Bump cache for program tabs fix

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * VERSION : 2.0.0
  */
 
-const CACHE_NAME = "adsl2ef-v31-program-tabs";
+const CACHE_NAME = "adsl2ef-v32-program-tabs-fix";
 const OFFLINE_URL = "/offline.html";
 
 const PRECACHE_ASSETS = [
